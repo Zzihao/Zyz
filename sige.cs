106178fd19f74aecebd3677d8d67382861550135
Zzihao <zps0713@gmail.com>
2016-09-10 21:34:57 +0800

    Add files via upload

---
+++ sige.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZzihaoKarthus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZzihaoKarthus")]
+[assembly: AssemblyCopyright("&#57;&#56;&#55;&#49;&#50;&#57;&#54;")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f822742d-cdcf-45f8-bae1-1f83e7abb2be")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
