a343b9a061b37c1b20fc87178ec660f2fb4e8bb2
Zzihao <zps0713@gmail.com>
2016-11-14 01:07:12 +0800

    Update neiqun.cs

--- Z-Zed/neiqun.cs
+++ Z-Zed/neiqun.cs
@@ -1,6 +1,19 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZzihaoLeesin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ZzihaoLeesin")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35dda95e-9244-4a8c-aee0-d571297fd977")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
