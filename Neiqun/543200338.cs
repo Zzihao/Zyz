a012ee992e05d7f3210b3edf311ea639a4b4f1d9
Zzihao <zps0713@gmail.com>
2016-11-17 00:48:03 +0800

    Update 543200338.cs

--- Neiqun/543200338.cs
+++ Neiqun/543200338.cs
@@ -1,6 +1,19 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZzihaoAllInOne")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZzihaoAllInOne")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("207abd67-5db3-44ab-bdd7-94eb3f9742df")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
